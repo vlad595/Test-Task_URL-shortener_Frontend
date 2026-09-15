0ef2e4356b551a2a7192d8d36d93e05094313abc
vlad595 <vladggaort@gmail.com>
2026-09-15 11:51:13 +0300

    Project inti

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -5,8 +5,7 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-    })
-      .compileComponents();
+    }).compileComponents();
   });
 
   it('should create the app', () => {
@@ -19,6 +18,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Test-Task_URL-shortener_Frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, Test-Task_URL-shortener_Frontend',
+    );
   });
 });
